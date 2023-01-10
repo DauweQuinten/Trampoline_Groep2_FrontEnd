be02e721ac3daeeecb5f8d5813d59a50266df679
DauweQuinten <quinten.dauwe@student.howest.be>
2023-01-10 14:01:45 +0100

    wsHandler script code

--- Assets/Scripts/WsHandler.cs
+++ Assets/Scripts/WsHandler.cs
@@ -1,13 +1,26 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using WebSocketSharp;
 
 public class WsHandler : MonoBehaviour
 {
-    // Start is called before the first frame update
+
+    WebSocket ws;
+
     void Start()
     {
         Debug.Log("Script started");
+
+        ws = new WebSocket("ws://172.30.248.72:3000");
+        ws.Connect();
+        
+        ws.OnMessage += (sender, e) =>
+        {
+            //Debug.Log($"Message received from {((WebSocket)sender).Url}");
+            Debug.Log("Message received: " + e.Data);            
+        };
+        
     }
 
     // Update is called once per frame
