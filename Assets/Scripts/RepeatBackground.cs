b11cbaf42b417ca1c25fde8dc652830634a59a09
DauweQuinten <quinten.dauwe@student.howest.be>
2023-01-12 16:24:37 +0100

    River endless scrolling

--- Assets/Scripts/RepeatBackground.cs
+++ Assets/Scripts/RepeatBackground.cs
@@ -4,15 +4,20 @@ using UnityEngine;
 
 public class RepeatBackground : MonoBehaviour
 {
+    Vector3 startPosition;
+        
     // Start is called before the first frame update
     void Start()
     {
-        
+        startPosition = transform.position;       
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(transform.position.z > startPosition.z + transform.localScale.z /2)
+        {
+            transform.position = startPosition;
+        }
     }
 }
