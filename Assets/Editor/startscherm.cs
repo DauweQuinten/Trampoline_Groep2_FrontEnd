21581b09a77743b0c8bf9f8fbd3f99a3ff800a89
jarneDel <jarne.delarue@student.howest.be>
2023-01-18 10:16:06 +0100

    test

---
+++ Assets/Editor/startscherm.cs
@@ -0,0 +1,38 @@
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.UIElements;
+using UnityEditor.UIElements;
+
+
+public class startscherm : EditorWindow
+{
+    [MenuItem("Window/UI Toolkit/startscherm")]
+    public static void ShowExample()
+    {
+        startscherm wnd = GetWindow<startscherm>();
+        wnd.titleContent = new GUIContent("startscherm");
+    }
+
+
+    public void CreateGUI()
+    {
+        // Each editor window contains a root VisualElement object
+        var root = rootVisualElement;
+
+        // VisualElements objects can contain other VisualElement following a tree hierarchy.
+        VisualElement label = new Label("Hello World! From C#");
+        root.Add(label);
+
+        // Import UXML
+        var visualTree = AssetDatabase.LoadAssetAtPath<VisualTreeAsset>("Assets/Editor/startscherm.uxml");
+        VisualElement labelFromUXML = visualTree.Instantiate();
+        root.Add(labelFromUXML);
+
+        // A stylesheet can be added to a VisualElement.
+        // The style will be applied to the VisualElement and all of its children.
+        var styleSheet = AssetDatabase.LoadAssetAtPath<StyleSheet>("Assets/Editor/startscherm.uss");
+        VisualElement labelWithStyle = new Label("Hello World! With Style");
+        labelWithStyle.styleSheets.Add(styleSheet);
+        root.Add(labelWithStyle);
+    }
+}
