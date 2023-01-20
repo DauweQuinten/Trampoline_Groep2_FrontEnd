fc37544379758a1fb50bdc8a68e98bb0b7d9a174
jarneDel <jarne.delarue@student.howest.be>
2023-01-20 11:21:56 +0100

    startscherm met reactive buttons

--- Assets/Editor/startscherm.cs
+++ Assets/Editor/startscherm.cs
@@ -13,29 +13,62 @@ public class startscherm : EditorWindow
         wnd.titleContent = new GUIContent("startscherm");
     }
 
-
     public void CreateGUI()
     {
         // Each editor window contains a root VisualElement object
         var root = rootVisualElement;
-
-        // VisualElements objects can contain other VisualElement following a tree hierarchy.
-        var styleSheet = AssetDatabase.LoadAssetAtPath<StyleSheet>("Assets/Editor/startscherm.uss");
-        root.styleSheets.Add(styleSheet);
-        
         // Import UXML
-        var visualTree = AssetDatabase.LoadAssetAtPath<VisualTreeAsset>("Assets/Editor/startscherm.uxml");
-        VisualElement labelFromUxml = visualTree.Instantiate();
+        var tvisualTree = AssetDatabase.LoadAssetAtPath<VisualTreeAsset>("Assets/Editor/startscherm.uxml");
+        VisualElement labelFromUxml = tvisualTree.Instantiate();
         root.Add(labelFromUxml);
+        ListenToButtons();
     }
-    
+
+    private void ListenToButtons()
+    {
+        var gameObject = GameObject.Find("SocketController");
+        var events = gameObject.GetComponent<SocketEvents>();
+        events.btnPressedLeft.AddListener(LeftMoveDown);
+        events.btnPressedRight.AddListener(RightMoveDown);
+        events.btnPressedBoth.AddListener(BothCloseUI);
+        // events.btnReleasedLeft.AddListener(LeftMoveUp);
+        // events.btnReleasedRight.AddListener(RightMoveUp);
+    }
+
+    private void BothCloseUI()
+    {
+        rootVisualElement.Clear();
+    }
+
+    private void RightMoveDown()
+    {
+        // select right button from visualtree
+        rootVisualElement.Query("yellowButton").First().AddToClassList("move-down");
+    }
+
+    private void LeftMoveDown(Color arg0)
+    {
+        // throw new System.NotImplementedException();
+        rootVisualElement.Query("blueButton").First().AddToClassList("move-down");
+    }
+
+    private void LeftMoveUp()
+    {
+        rootVisualElement.Query("blueButton").First().RemoveFromClassList("move-down");
+    }
+
+    private void RightMoveUp()
+    {
+        rootVisualElement.Query("yellowButton").First().RemoveFromClassList("move-down");
+    }
+
     void OnEnable()
     {
         CreateGUI();
     }
+
     void OnDisable()
     {
         rootVisualElement.Clear();
     }
-    
 }
