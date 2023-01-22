6291512f152d65bb47535676b432d62c40bf1507
JarneDelarue <jarne.delarue@student.howest.be>
2023-01-22 15:51:55 +0100

    finished startscherm button events

--- Assets/Scripts/UiScripts/startschermManager.cs
+++ Assets/Scripts/UiScripts/startschermManager.cs
@@ -1,5 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
+using Models;
+using UiScripts;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UIElements;
@@ -11,10 +13,7 @@ public class startschermManager : MonoBehaviour
     private VisualElement _btnYellowTop;
     private VisualElement _btnBlueTop;
 
-    private bool _leftDown;
-    private bool _rightDown;
-    private bool _destroy;
-    private bool _btnUpdate;
+    private int _previousUpdateCount = -1;
     private bool _isEnabled;
 
 
@@ -27,91 +26,34 @@ public class startschermManager : MonoBehaviour
         _btnYellowTop = btnYellow.Q("buttonTop");
         var btnBlue = _document.rootVisualElement.Q("blueButton");
         _btnBlueTop = btnBlue.Q("buttonTop");
-        ListenToButtons();
+        ButtonListener.ListenToButtons();
         
     }
 
     private void Update()
     {
-        if (_btnUpdate && _isEnabled)
+        if (!_isEnabled || ButtonListener.BtnUpdate <= _previousUpdateCount) return;
+        if (ButtonListener.Both == BtnValue.Pressed)
         {
-            _btnUpdate = false;
-            if (_leftDown)
-            {
-                _btnBlueTop.AddToClassList("move-down");
-            }
-            else
-            {
-                _btnBlueTop.RemoveFromClassList("move-down");
-            }
-
-            if (_rightDown)
-            {
-                _btnYellowTop.AddToClassList("move-down");
-            }
-            else
-            {
-                _btnYellowTop.RemoveFromClassList("move-down");
-            }
-
-            if (_destroy)
-            {
-                _document.rootVisualElement.Clear();
-                SceneManager.LoadScene("BoatGame");
-            }
+            _document.rootVisualElement.Clear();
+            SceneManager.LoadScene("BoatGame");
         }
-    }
-
-    private void ListenToButtons()
-    {
-        // link to socketController Events
-        var socketObject = GameObject.Find("SocketController");
-        var events = socketObject.GetComponent<SocketEvents>();
-        events.btnPressedLeft2.AddListener(LeftMoveDown);
-        events.btnPressedLeft.AddListener(LeftMoveDown2);
-        events.btnPressedRight.AddListener(RightMoveDown);
-        events.btnReleasedLeft.AddListener(LeftMoveUp);
-        events.btnReleasedRight.AddListener(RightMoveUp);
-        events.btnPressedBoth.AddListener(BothButtonsOnClicked);
-    }
-
-    private void LeftMoveDown2(Color arg0)
-    {
-        _leftDown = true;
-        _btnUpdate = true;
-    }
-
-    private void RightMoveUp()
-    {
-        _rightDown = false;
-        _btnUpdate = true;
-    }
-
-    private void LeftMoveUp()
-    {
-        _leftDown = false;
-        _btnUpdate = true;
-
-    }
-
-    private void RightMoveDown()
-    {
-        _rightDown = true;
-        _btnUpdate = true;
-
-    }
-
-    private void LeftMoveDown()
-    {
-        _leftDown = true;
-        _btnUpdate = true;
-
-    }
-
-    private void BothButtonsOnClicked()
-    {
-        _destroy = true;
-        _btnUpdate = true;
-
+        else if (ButtonListener.Left == BtnValue.Pressed)
+        {
+            _btnBlueTop.AddToClassList("move-down");
+        }
+        else if (ButtonListener.Right == BtnValue.Pressed)
+        {
+            _btnYellowTop.AddToClassList("move-down");
+        }
+        else if (ButtonListener.Left == BtnValue.Released)
+        {
+            _btnBlueTop.RemoveFromClassList("move-down");
+        }
+        else if (ButtonListener.Right == BtnValue.Released)
+        {
+            _btnYellowTop.RemoveFromClassList("move-down");
+        }
+        _previousUpdateCount = ButtonListener.BtnUpdate;
     }
 }
