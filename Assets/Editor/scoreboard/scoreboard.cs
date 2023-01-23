81f73dc69b44cab53fe8ac2f117396c9c459394d
Dago <dagobert.harinck@student.howest.be>
2023-01-23 09:14:11 +0100

    push

--- Assets/Editor/scoreboard/scoreboard.cs
+++ Assets/Editor/scoreboard/scoreboard.cs
@@ -8,6 +8,8 @@ using static UnityEditor.Progress;
 using System.Collections.Generic;
 using Models;
 using System;
+using System.Collections;
+using UnityEngine.Networking;
 
 public class scoreboard : EditorWindow
 {
@@ -50,17 +52,53 @@ public class scoreboard : EditorWindow
         list_Items.Sort();
         Action<VisualElement, int> bindItem = (e, i) => (e as Label).text = $"{list_Items[i].Username} - {list_Items[i].Score}";
         lvwScores = root.Q<ListView>("lvwScores");
-        
+
         Func<VisualElement> makeItem = () =>
         {
+            var v = new VisualElement();
+            var i = new Image();
             var l = new Label();
             l.style.width = 64;
             l.style.color = Color.white;
-            return l;
+            v.Add(i);
+            v.Add(l);
+            return v;
         };
 
         lvwScores.makeItem = makeItem;
         lvwScores.bindItem = bindItem;
         lvwScores.itemsSource = list_Items;
+    }
+
+
+    public static async Task<Texture2D> GetRemoteTexture(string url)
+    {
+        using (UnityWebRequest www = UnityWebRequestTexture.GetTexture(url))
+        {
+            // begin request:
+            var asyncOp = www.SendWebRequest();
+
+            // await until it's done: 
+            while (asyncOp.isDone == false)
+                await Task.Delay(1000 / 30);//30 hertz
+
+            // read results:
+            //if (www.isNetworkError || www.isHttpError)
+            if( www.result!=UnityWebRequest.Result.Success )// for Unity >= 2020.1
+            {
+                // log error:
+                #if DEBUG
+                Debug.Log($"{www.error}, URL:{www.url}");
+                #endif
+
+                // nothing to return on error:
+                return null;
+            }
+            else
+            {
+                // return valid results:
+                return DownloadHandlerTexture.GetContent(www);
+            }
+        }
     }
 }
