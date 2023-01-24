ea94b16bd9ac4c16df6ed4825446077a01b38b2b
DauweQuinten <quinten.dauwe@student.howest.be>
2023-01-24 10:33:25 +0100

    added distance percentage

--- Assets/Scripts/LevelController.cs
+++ Assets/Scripts/LevelController.cs
@@ -15,13 +15,32 @@ public class LevelController : MonoBehaviour
     public bool gameOver = false;
     private int score = 0;
     private float timeToScore = 0.5f;
+    
+    private float distanceAtStart;
+    private float distanceToShark;
+    public float distancePercentage;
+
+    // GameObjects
+    private GameObject player;
+    private GameObject shark;
+
 
 
     private void Start()
     {
+        #region initialize events
+        
         onGameOver ??= new UnityEvent();
 
+        #endregion
 
+        #region get game objects
+
+        player = GameObject.Find("Boat");
+        shark = GameObject.Find("Shark");
+
+        #endregion
+  
         #region startup logging
 
         Debug.Log("Game started!");
@@ -30,6 +49,10 @@ public class LevelController : MonoBehaviour
         Debug.Log($"right player's index is: {GameVariablesHolder.playerMapping[1]}");
 
         #endregion
+
+
+        // Get initial distance between player and shark
+        distanceAtStart = Vector3.Distance(player.transform.position, shark.transform.position);
 
         StartCoroutine(addScore(timeToScore));
     }
@@ -43,17 +66,21 @@ public class LevelController : MonoBehaviour
             Debug.Log($"Score: {score}");
         }
     }
-
-
+  
     private void Update()
     {
+        // calculate distance between shark and player if the player is not dead
+        if (player && shark)
+        {
+            distanceToShark = Vector3.Distance(player.transform.position, shark.transform.position);
+            distancePercentage = MathF.Floor((distanceToShark / distanceAtStart)*100);        
+        }
+        
         if (gameOver == true)
         {
             GameVariablesHolder.Score = score;
             onGameOver.Invoke();
-
-            // SceneManager.LoadScene(""); 
-
+            SceneManager.LoadScene("Game-over"); 
         }
         
     }
