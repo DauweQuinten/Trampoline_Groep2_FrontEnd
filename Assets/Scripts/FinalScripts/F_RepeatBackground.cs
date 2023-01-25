f1d8b84f7cd80f8f24c8d61298e9d86ba313fa45
DauweQuinten <quinten.dauwe@student.howest.be>
2023-01-25 10:37:27 +0100

    Added move forward to river

---
+++ Assets/Scripts/FinalScripts/F_RepeatBackground.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class F_RepeatBackground : MonoBehaviour
+{
+    Vector3 startPosition;
+    BoxCollider boxCollider;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        startPosition = transform.position;
+        boxCollider = GetComponent<BoxCollider>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (transform.position.z > startPosition.z + boxCollider.size.x/2)
+        {
+            transform.position = startPosition;
+        }
+    }
+}
